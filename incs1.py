b79984145b9bfbb542f5ead0693652a67b58f4d8
Ganjaisaikumar <112651698+Ganjaisaikumar@users.noreply.github.com>
2022-10-20 13:52:01 -0500

    Add files via upload

---
+++ incs1.py
@@ -0,0 +1,9 @@
+import numpy as np
+array=np.random.randint(1,20,(1,15))
+print(array)
+print("-------")
+array=np.reshape(array,(3,5))
+print(array)
+for i in array:
+    i[np.where(i==i.max())]=0
+print(array)
